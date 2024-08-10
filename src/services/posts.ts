866f13fb45f1ac36686b1866f0c26784675a7f50
juanRCoder <guillecarck@gmail.com>
2024-08-10 13:14:57 -0500

    feat: slice posts

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,11 +1,10 @@
 export async function getPosts() {
   const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return data.json();
+  const posts = await data.json();
+  return posts.slice(0, 5);
 }
 
 export async function getPostsId(id: string) {
-    const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    return data.json();
-  }
-    
-  
+  const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return data.json();
+}
